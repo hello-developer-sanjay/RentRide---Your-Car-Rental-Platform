d9ea281e495b1dd2716a244e017012b983c61d94
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-10-13 22:21:37 +0530

    Update setupProxy.js

--- seller-app-frontend/setupProxy.js
+++ seller-app-frontend/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://rent-nqhh.onrender.com',
+      target: 'https://rentme-0vht.onrender.com',
       changeOrigin: true,
     })
   );
